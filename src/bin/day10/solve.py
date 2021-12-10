4f290ab09fc974775fb48bfa304046db2095683e
Andrew Barbarello <andrew.barbarello@outlook.com>
2021-12-10 08:26:19 -0500

    Cleanup day 10 python solution

--- src/bin/day10/solve.py
+++ src/bin/day10/solve.py
@@ -1,6 +1,7 @@
 import argparse
 from enum import Enum
 from functools import reduce
+from typing import Optional
 
 PAIRS = {
     '(': ')',
@@ -8,8 +9,6 @@ PAIRS = {
     '{': '}',
     '<': '>',
 }
-
-INVERSE_PAIRS = {v:k for k, v in PAIRS.items()}
 
 ERROR_SCORE = {
     ')': 3,
@@ -19,56 +18,44 @@ ERROR_SCORE = {
 }
 
 COMPLETION_SCORE = {
-    ')': 1,
-    ']': 2,
-    '}': 3,
-    '>': 4,
+    '(': 1,
+    '[': 2,
+    '{': 3,
+    '<': 4,
 }
 
-def get_error_score(line):
+class ScoreType(Enum):
+    ERROR = 1
+    COMPLETION = 2
+
+def get_score(line, score_type: ScoreType) -> Optional[int]:
     stack = []
     for c in line:
         if c in PAIRS:
             stack.append(c)
         elif stack and c == PAIRS[stack[-1]]:
             stack.pop()
-        elif stack:
+        elif score_type == ScoreType.ERROR:
             #this was an invalid delimeter, add to score
             return ERROR_SCORE[c]
+        else:
+            return None
+
+    if score_type == ScoreType.COMPLETION:
+        return reduce(lambda acc, c: 5*acc + COMPLETION_SCORE[c], reversed(stack), 0)
 
 def part1(lines):
-    total_score = 0
-    incomplete_lines = []
-    for l in lines:
-        score = get_error_score(l)
-        if score is None:
-            incomplete_lines.append(l)
-        else:
-            total_score += score
-
-    return total_score, incomplete_lines
-
-
-def get_incomplete_score(line):
-    stack = []
-    for c in line:
-        if c in PAIRS:
-            stack.append(c)
-        elif stack and c == PAIRS[stack[-1]]:
-            stack.pop()
-        else:
-            assert False, "This is a corrupt line!"
-    completion = ''.join(PAIRS[c] for c in reversed(stack))
-    return reduce(lambda acc, c: 5*acc + COMPLETION_SCORE[c], completion, 0)
-
+    scores = [get_score(l, ScoreType.ERROR) for l in lines]
+    return sum(s for s in scores if s is not None)
 
 def part2(lines):
-    scores = [get_incomplete_score(l) for l in lines]
+    scores = [get_score(l, ScoreType.COMPLETION) for l in lines]
+    scores = [s for s in scores if s is not None]
     scores.sort()
     return scores[len(scores)//2]
 
 def main():
-    parser = argparse.ArgumentParser(description='Solution for day 9 of aoc2021')
+    parser = argparse.ArgumentParser(description='Solution for day 10 of aoc2021')
     parser.add_argument('input_file')
 
     args = parser.parse_args()
@@ -76,10 +63,8 @@ def main():
     with open(args.input_file) as f:
         lines = [l for l in f.read().splitlines() if l.strip()]
 
-    p1_score, incomplete = part1(lines)
-    print(p1_score)
-
-    print(part2(incomplete))
+    print(part1(lines))
+    print(part2(lines))
 
 if __name__ == '__main__':
     main()
