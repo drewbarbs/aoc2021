cbf223eaa072a850b3162b1d7f142d588cb49dd6
Andrew Barbarello <andrew.barbarello@outlook.com>
2021-12-24 08:45:29 -0500

    Finish python solution for day 22

--- src/bin/day22/solve.py
+++ src/bin/day22/solve.py
@@ -1,49 +1,119 @@
 import argparse
-import bisect
+import functools
 import re
+
 
 LINE_REGEX = re.compile('^(on|off) x=([-0-9]+)\\.\\.([-0-9]+),y=([-0-9]+)\\.\\.([-0-9]+),z=([-0-9]+)\\.\\.([-0-9]+)$')
 
 
-def get_intersection(intervals, query):
-    intersection = []
-    if not intervals:
-        return intersection
+def get_intersection_interval(a, b):
+    astart, aend = a
+    bstart, bend = b
 
-    lo, hi = query
+    if astart <= bstart and aend >= bstart:
+        return bstart, min(aend, bend)
+    elif astart > bstart and astart <= bend:
+        return astart, min(aend, bend)
 
-    idx = bisect.bisect_left(intervals, ((lo, float('-inf')), []))
-    if idx > 0 and intervals[idx-1][0][1] >= lo:
-        intersection.append(intervals[idx-1])
 
-    # add intervals this one extends past
-    for i in range(idx, len(intervals)):
-        if hi >= intervals[i][0][0]:
-            intersection.append(intervals[i])
+def get_intersection_volume(a, b):
+    a_xrange, a_yrange, a_zrange = a
+    b_xrange, b_yrange, b_zrange = b
+
+    if xoverlap := get_intersection_interval(a_xrange, b_xrange):
+        if yoverlap := get_intersection_interval(a_yrange, b_yrange):
+            if zoverlap := get_intersection_interval(a_zrange, b_zrange):
+                return xoverlap, yoverlap, zoverlap
+
+
+def get_volume(box):
+    return functools.reduce(lambda a, b: a*b, (abs(dim[1] - dim[0]) + 1 for dim in box))
+
+
+def get_difference_volumes(vol, sub_vol):
+    """ Return the parts of vol not in sub_vol """
+    if not vol:
+        return []
+
+    difference_volumes = []
+    if pre_int := get_intersection_interval(vol[0], (float('-inf'), sub_vol[0][0] - 1)):
+        difference_volumes.append((pre_int,) + vol[1:])
+
+    if post_int := get_intersection_interval(vol[0], (sub_vol[0][1] + 1, float('inf'))):
+        difference_volumes.append((post_int,) + vol[1:])
+
+    intersect_int = get_intersection_interval(vol[0], sub_vol[0])
+    for sub_diff in get_difference_volumes(vol[1:], sub_vol[1:]):
+        difference_volumes.append((intersect_int,) + sub_diff)
+
+    return difference_volumes
+
+
+def add_to_volumes(volumes, to_add):
+    overlaps = []
+    for v in volumes:
+        if overlap := get_intersection_volume(v, to_add):
+            overlaps.append(v)
+
+    addition_volumes = [to_add]
+    for overlap in overlaps:
+        addition_volumes = remove_from_volumes(addition_volumes, overlap)
+
+    new_volumes = volumes[:]
+    new_volumes.extend(addition_volumes)
+
+    return new_volumes
+
+
+def do_remove(volumes, removals):
+    for n_processed, v in enumerate(volumes):
+        for j, removal in enumerate(removals):
+            if overlap := get_intersection_volume(v, removal):
+                unprocessed_volumes = get_difference_volumes(v, overlap)
+                unprocessed_volumes.extend(volumes[n_processed+1:])
+                new_removals = removals[:j]
+                new_removals.extend(get_difference_volumes(removal, overlap))
+                new_removals.extend(removals[j+1:])
+                return volumes[:n_processed], unprocessed_volumes, new_removals
+
+    return volumes, [], []
+
+
+def remove_from_volumes(volumes, to_remove):
+    final_volumes = []
+    removals = [to_remove]
+    unprocessed = volumes
+    while removals:
+        processed, unprocessed, removals = do_remove(unprocessed, removals)
+        final_volumes.extend(processed)
+
+    final_volumes.extend(unprocessed)
+
+    return final_volumes
+
+
+def run_seq(instrs):
+    boxes = []
+
+    for (action, x_range, y_range, z_range) in instrs:
+        box = (x_range, y_range, z_range)
+        if action == 'on':
+            boxes = add_to_volumes(boxes, box)
         else:
-            break
+            assert action == 'off'
+            boxes = remove_from_volumes(boxes, box)
 
-    return intersection
+    return sum(get_volume(b) for b in boxes)
 
 
 def part1(instrs):
-    turned_on = []
+    def in_range(r):
+        return abs(r[0]) <= 50 and abs(r[1]) <= 50
 
-    for (action, (x_min, x_max), (y_min, y_max), (z_min, z_max)) in instrs:
-        if action == 'on':
-            intersecting_x = get_intersection(turned_on, (x_min, x_max))
-            if not intersecting_x:
-                insert = bisect.bisect_left(turned_on, ((x_min, x_max), []))
-                turned_on.insert(insert, ((x_min, x_max), [((y_min, y_max), [((z_min, z_max), [])])]))
-            else:
-                while intersecting_x:
-                    ((fst_min_x, fst_max_x), fst_squares) = intersecting_x[0]
-                    if fst_min_x == x_min and fst_max_x == x_max:
-                        # now we just need to insert new squares into fst_squares
-                        pass
+    instrs = [(act, x_r, y_r, z_r) for act, x_r, y_r, z_r in instrs
+              if in_range(x_r) and in_range(y_r) and in_range(z_r)]
 
-                    # split the range
-                    pass
+    return run_seq(instrs)
 
 
 def parse_input(s):
@@ -66,7 +136,8 @@ def main():
     with open(args.input_file, 'r', encoding='utf8') as f:
         instrs = parse_input(f.read())
 
-    part1(instrs)
+    print(part1(instrs))
+    print(run_seq(instrs))
 
 
 if __name__ == '__main__':
